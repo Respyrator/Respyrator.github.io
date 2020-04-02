5d6f074c01915ecd18f1bdfc79909eef72bb06ef
Israel Blancas <iblancasa@gmail.com>
2020-04-03 00:19:24 +0200

    #25: añade licencia a las cabeceras

--- src/sercomm/comm.py
+++ src/sercomm/comm.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2020 Respyrador
+# This file is part of Respyrador <https://respyrator.github.io/respirador/>.
+#
+# Respyrador is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Respyrador is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Respyrador.  If not, see <http://www.gnu.org/licenses/>.
+
 # Built-in --------------------------------------------------------------------
 from time import sleep, time
 # Installed -------------------------------------------------------------------
